a8af267d744085171530b6c8db5635c12199b2d4
Arjun Prakash <archieoi@gmail.com>
2011-05-19 00:49:36 -0700

    WordATron 0.01

--- WordATron/main.cpp
+++ WordATron/main.cpp
@@ -3,9 +3,9 @@
 //  WordATron
 //
 //  Created by arjun prakash on 2011-05-19.
-//  Copyright 2011 bellmonde. All rights reserved.
+//  Copyright 2011 cyborgDino. All rights reserved.
 //
-
+/*
 #include <iostream>
 
 int main (int argc, const char * argv[])
@@ -15,4 +15,52 @@ int main (int argc, const char * argv[])
     std::cout << "Hello, World!\n";
     return 0;
 }
+*/
 
+
+#include<iostream>
+#include<boost/tokenizer.hpp>
+#include<string>
+#include <fstream>
+
+void readFile();
+void readText();
+
+int main()
+{
+    using namespace std;
+    using namespace boost;
+    readFile();
+    //readText();
+    cout << "WordATron 0.01" << endl;
+    
+}
+
+void readFile() {
+    
+    using namespace std;
+    using namespace boost;
+    
+    ifstream file( __FILE__ ) ;
+    string str;
+
+    while( getline( file, str ) )
+    {
+        tokenizer<> toker( str ) ;
+        for( tokenizer<>::iterator beg = toker.begin(); 
+            beg != toker.end() ; ++beg ) cout << *beg << '\n' ;
+    }
+}
+
+void readText() {
+    
+    using namespace std;
+    using namespace boost;
+    
+    string s = "This is, a test";
+    tokenizer<> tok(s);
+    for(tokenizer<>::iterator beg=tok.begin(); beg!=tok.end(); ++beg) {
+        cout << *beg << "\n";
+    }
+
+}
