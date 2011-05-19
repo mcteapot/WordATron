4cf1513fd29e5b060d5e0b4bdc88757f6971ef11
Arjun Prakash <archieoi@gmail.com>
2011-05-19 00:23:54 -0700

    WordATron 0.011

---
+++ WordATron/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WordATron
+//
+//  Created by arjun prakash on 2011-05-19.
+//  Copyright 2011 bellmonde. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
